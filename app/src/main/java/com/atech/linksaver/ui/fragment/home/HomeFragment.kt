4246a3ed2cbec73d1e18062ac0a7488f8eb70c89
aiyu-ayaan <aiyuayaan22@gmail.com>
2023-05-27 16:19:27 +0530

    HomeScreen UI part done

--- app/src/main/java/com/atech/linksaver/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/atech/linksaver/ui/fragment/home/HomeFragment.kt
@@ -6,8 +6,9 @@ import android.viewbinding.library.fragment.viewBinding
 import androidx.fragment.app.Fragment
 import com.atech.linksaver.R
 import com.atech.linksaver.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private val binding : FragmentHomeBinding by viewBinding()
 
